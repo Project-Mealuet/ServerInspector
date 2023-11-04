b48f1f31af6177b48f16b2fe95ff38571711e8d4
Shiki@Z <kressety@163.com>
2023-11-04 12:15:32 +0000

    使用jsonify处理

--- modules/mc_status.py
+++ modules/mc_status.py
@@ -6,8 +6,22 @@ def mc_status():
     try:
         with Client('127.0.0.1', 25585) as client:
             full_status = client.stats(full=True)
+            return jsonify({
+                'host_name': full_status.host_name,
+                'game_type': full_status.game_type,
+                'game_id': full_status.game_id,
+                'version': full_status.version,
+                'plugins': full_status.plugins,
+                'map': full_status.map,
+                'host_port': full_status.host_port,
+                'player': {
+                    'num': full_status.num_players,
+                    'max': full_status.max_players,
+                    'list': full_status.players
+                }
+            })
     except ConnectionRefusedError:
-        full_status = {
+        return {
             'host_name': '',
             'game_type': '',
             'game_id': '',
@@ -22,17 +36,3 @@ def mc_status():
             }
         }
 
-    return jsonify({
-        'host_name': full_status.host_name,
-        'game_type': full_status.game_type,
-        'game_id': full_status.game_id,
-        'version': full_status.version,
-        'plugins': full_status.plugins,
-        'map': full_status.map,
-        'host_port': full_status.host_port,
-        'player': {
-            'num': full_status.num_players,
-            'max': full_status.max_players,
-            'list': full_status.players
-        }
-    })
