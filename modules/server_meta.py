6eba370eb692a02fbc99fbfa43a9c1c28046b7d2
Shiki@Z <kressety@163.com>
2023-11-04 17:40:53 +0000

    路径不存在时返回默认值

--- modules/server_meta.py
+++ modules/server_meta.py
@@ -1,10 +1,17 @@
-from os.path import join
+from os.path import join, exists
 
-from flask import send_from_directory
+from flask import send_from_directory, jsonify
 
 GAME_ROOT = '/home/ubuntu'
 
 
 def server_meta(sub_path):
     full_path = join(GAME_ROOT, sub_path)
+    if not exists(full_path):
+        return jsonify({
+            'name': '',
+            'logo': '',
+            'game_version': '',
+            'mod_loader': ''
+        })
     return send_from_directory(full_path, 'server_meta.json')
